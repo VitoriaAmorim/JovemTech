4b37fe0d751460892b3d605b6c02402fd78c0910
VitoriaAmorim <vitoriajuliana118@gmail.com>
2023-02-10 16:16:17 -0300

    10-02-23

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
 
 var express = require ('express');
 var app = express ();
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const Comment = require('./models/comment')
+//Config.
+    //Template Engine
+    app.engine('handlebars',handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+//Body Parser
+    app.use(bodyParser.urlencoded({extended:false}))
+    app.use(bodyParser.json())
+//Rotas Inicial
+        app.get('/',function(req,res){
+            Comment.findAll({order: [['id','DESC']]}).then(function(comments){
+                res.render('home', {comment:comments})
+            })
+                })
+//Rota do formulário
+        app.get('/form',function(req,res){
+            res.render('formulário.handlebars')
+        })
+//rotas "post" só pode ser acessada quando alguem faz uma requisição
+        app.post('/add',function(req,res){
+            Comment.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo 
+            }).then(function(){
+                res.redirect('/')
+            }).catch(function(erro){
+                res.send("Comentário não pode ser criado" + erro)
+            })
+        })
+
+        //Rota para deletar Comentários
+        app.get('/deletar/:id',function(req,res){
+            Comment.destroy({where:{'id':req.params.id}}).then(function(){
+                res.send("Comentário deletado com sucesso")
+            }).catch(function(erro){
+                res.send('Ocorreu um erro')
+            })
+        })
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
